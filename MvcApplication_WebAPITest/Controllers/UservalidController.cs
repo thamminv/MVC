bfa624c69c4f870db4cbb5e94ce46621b2110547
AvinashAttipalli <attipalli.avinash@gmail.com>
2017-05-26 07:57:24 -0400

    First MVC COmmit

---
+++ MvcApplication_WebAPITest/Controllers/UservalidController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MvcApplication_WebAPITest.Controllers
+{
+    public class UservalidController : ApiController
+    {
+
+        public uservaliddetails Get(int id)
+        {
+            var result = new uservaliddetails();
+            result.Id = id;
+            result.Status = false;
+
+            if (id < 6)
+            {                
+                result.Status = true;
+            }
+
+            return result;
+        }
+
+        public class uservaliddetails
+        {
+            public int Id { get; set; }
+            public Boolean Status { get; set; }
+        }
+
+    }
+}
---
+++ MvcApplication_WebAPITest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcApplication_WebAPITest.WebApiApplication" Language="C#" %>
